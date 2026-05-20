f56d174c3d423591529ff952a2540ee569875670
boywonder0806 <boywonder0806@gmail.com>
2026-05-20 12:03:55 -0500

    fix ssl cert for DigitalOcean managed postgres

--- server/src/db/index.js
+++ server/src/db/index.js
@@ -1,6 +1,8 @@
 import pg from 'pg';
 
-// Will be used once DATABASE_URL is configured on DigitalOcean
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
 
 export default pool;
